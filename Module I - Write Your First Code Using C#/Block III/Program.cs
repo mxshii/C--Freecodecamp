a9f9d48b770340eabd37338fea130108e325dccf
mxshii <omarsherif960@hotmail.com>
2025-06-23 19:13:47 +0300

    finished

--- Module I - Write Your First Code Using C#/Block III/Program.cs
+++ Module I - Write Your First Code Using C#/Block III/Program.cs
@@ -1,4 +1,9 @@
-﻿//Unit 2 of 8 Combine strings using character escape sequences
+﻿//To Encode Unicode
+Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+
+
+//Unit 2 of 8 Combine strings using character escape sequences
 
 /*
 Character escape sequences
@@ -9,7 +14,6 @@ the escape character sequence begins with a backslash \ followed by the characte
 For example, the \n sequence will add a new line, and a \t sequence will add a tab.
 */
 
-using System.Reflection.PortableExecutable;
 
 Console.WriteLine("Hello\nWorld");
 Console.WriteLine("Hello\tWorld");
@@ -63,3 +67,35 @@ string secondName = "Mazen";
 string greetings = "Hello";
 string msg = greetings + " " + secondName + "!";
 Console.WriteLine(msg);
+
+//Unit 4 of 8 Combine strings using string interpolation
+
+/*
+What is string interpolation?
+String interpolation combines multiple values into a single literal string by using a "template" 
+and one or more interpolation expressions.
+An interpolation expression is indicated by an opening and closing curly brace symbol { }. 
+You can put any C# expression that returns a value inside the braces. 
+The literal string becomes a template when it's prefixed by the $ character.
+*/
+
+string thirdName = "Youssef";
+string updateMessage = "Update to Windows 11";
+Console.WriteLine($"{thirdName} {updateMessage}");
+
+//Quick note you can put interpolation and Verbatim string together
+
+Console.WriteLine($@"{thirdName} \ {updateMessage}");
+
+//Unit 5 of 8 Complete the challenge
+
+string projectName = "ACME";
+
+string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
+string englishMessage = "View English output:";
+
+Console.WriteLine($@"{englishMessage}
+c:\Exercise\{projectName}\data.txt");
+
+Console.WriteLine($@"{russianMessage}:
+c:\Exercise\{projectName}\ru-RU\data.txt");
